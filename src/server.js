d6768d4b3300f362ab754a3e9989331c07bacc98
skotovskij <semyonkotovsky@Semyons-MacBook-Pro.local>
2024-08-02 13:42:09 +0300

    Enabled cors

--- src/server.js
+++ src/server.js
@@ -2,14 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
+const cors = require('cors');
+
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', routes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-// TODO: Implement sigma lints
