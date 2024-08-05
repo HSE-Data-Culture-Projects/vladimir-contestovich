faf081d75020e29ae96602769ffb8691c60b23c4
skotovskij <semyonkotovsky@Semyons-MacBook-Pro.local>
2024-08-05 14:00:44 +0300

    Added logger

--- src/server.js
+++ src/server.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
 const logger = require('./logger');
 
 const app = express();
