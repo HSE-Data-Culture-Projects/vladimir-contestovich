6531f88fc1483c7685a08bada6107a43b6f6eb4f
skotovskij <semyonkotovsky@Semyons-MacBook-Pro.local>
2025-04-08 13:44:17 +0300

    Update env

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const logger = require('./logger');
 const { TOKEN } = require('./config');
 
 const app = express();
-const port = 3002;
+const port = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
